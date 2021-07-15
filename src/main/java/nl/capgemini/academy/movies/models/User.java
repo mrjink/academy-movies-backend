0c74e0006eb1e7e1db05e3dbaadf5a42b8c24c86
Gillis J. de Nijs <gillis.de-nijs@capgemini.com>
2021-07-15 19:30:05 +0200

    Add User and UserRepository.
    Enable /h2-console
    Make database username and password explicit.

---
+++ src/main/java/nl/capgemini/academy/movies/models/User.java
@@ -0,0 +1,53 @@
+package nl.capgemini.academy.movies.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(uniqueConstraints = @UniqueConstraint(name = "UNIQUE_KEY_USERNAME", columnNames = "username"))
+public class User {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String username;
+    private String password;
+    private boolean enabled;
+
+    public User() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+}
---
+++ src/main/java/nl/capgemini/academy/movies/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package nl.capgemini.academy.movies.repositories;
+
+import nl.capgemini.academy.movies.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
