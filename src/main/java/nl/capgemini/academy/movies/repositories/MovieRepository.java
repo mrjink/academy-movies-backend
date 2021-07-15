2734e4e43f74afb90a6e38bdb4b48167e7e41505
Gillis J. de Nijs <gillis.de-nijs@capgemini.com>
2021-07-15 19:26:32 +0200

    Add Repository annotation.

--- src/main/java/nl/capgemini/academy/movies/repositories/MovieRepository.java
+++ src/main/java/nl/capgemini/academy/movies/repositories/MovieRepository.java
@@ -3,6 +3,8 @@ package nl.capgemini.academy.movies.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import nl.capgemini.academy.movies.models.Movie;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 }
